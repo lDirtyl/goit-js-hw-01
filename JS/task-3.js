25dad257f41dede18c2ccc0b3cf289e87d98966b
lDirtyl <andrii.mashtaler@gmx.at>
2024-05-15 11:52:16 +0200

    change task-3 and add script to html

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,7 +1,7 @@
 function getElementWidth (content, padding, border) {
-    const contentWidth = Nummer.parseFloat(content);
-    const paddingWidth = Nummer.parseFloat(padding);
-    const borderWidth = Nummer.parseFloat(border);
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
 
     return contentWidth + 2 * paddingWidth + 2 * borderWidth
 }
